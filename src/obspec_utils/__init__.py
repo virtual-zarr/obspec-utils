9d30b9fc9e3716deb29b5f0d140c3472677a9669
Max Jones <14077947+maxrjones@users.noreply.github.com>
2025-11-21 16:34:00 -0500

    Add ObstoreMemCacheReader

--- src/obspec_utils/__init__.py
+++ src/obspec_utils/__init__.py
@@ -1,4 +1,4 @@
 from ._version import __version__
-from .file_handlers import ObstoreReader
+from .file_handlers import ObstoreMemCacheReader, ObstoreReader
 
-__all__ = ["__version__", "ObstoreReader"]
+__all__ = ["__version__", "ObstoreMemCacheReader", "ObstoreReader"]
