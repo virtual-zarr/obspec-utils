e834b1c7ee0f1e0f2cd7cdf372a47ccc59d19b49
Max Jones <14077947+maxrjones@users.noreply.github.com>
2025-12-18 15:46:51 -0600

    Add ObjectStoreRegistry

--- src/obspec_utils/__init__.py
+++ src/obspec_utils/__init__.py
@@ -1,4 +1,10 @@
 from ._version import __version__
 from .file_handlers import ObstoreMemCacheReader, ObstoreReader
+from .registry import ObjectStoreRegistry
 
-__all__ = ["__version__", "ObstoreMemCacheReader", "ObstoreReader"]
+__all__ = [
+    "__version__",
+    "ObstoreMemCacheReader",
+    "ObstoreReader",
+    "ObjectStoreRegistry",
+]
